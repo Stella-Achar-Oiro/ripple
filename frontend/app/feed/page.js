bcf8ed60be07059cd9b22e343975fb5865c13fa1
Maurice <ombewamaurice79@gmail.com>
2025-07-12 18:14:36 +0300

    Comment out SuggestedGroups import in feed page

--- frontend/app/feed/page.js
+++ frontend/app/feed/page.js
@@ -5,7 +5,7 @@ import RouteGuard from '../../components/Auth/RouteGuard'
 import MainLayout from '../../components/Layout/MainLayout'
 import CreatePost from '../../components/Feed/CreatePost'
 import OnlineFriends from '../../components/Widgets/OnlineFriends'
-import SuggestedGroups from '../../components/Widgets/SuggestedGroups'
+// import SuggestedGroups from '../../components/Widgets/SuggestedGroups'
 import PostList from '../../components/Feed/PostList'
 import styles from './page.module.css'
 
